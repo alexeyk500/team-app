ba286690d5d24857c8a994883cd26a9ac5710e2f
alexeyk500 <alexeyk500@yandex.ru>
2023-05-02 13:34:01 +0300

    new index.html

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
+import 'normalize.css';
 import './index.css';
+
+console.log(`team-app version = 1.0.0`);
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
