2247c6a0dc00e153442068e97422a71f1a02d543
alexeyk500 <alexeyk500@yandex.ru>
2023-05-02 18:28:00 +0300

    cards - done

--- src/Components/TeamComponent/TeamHeadersSection/TeamHeadersSection.tsx
+++ src/Components/TeamComponent/TeamHeadersSection/TeamHeadersSection.tsx
@@ -1,9 +1,34 @@
 import React from 'react';
 import TeamHeaderCard from './TeamHeaderCard/TeamHeaderCard';
+import classes from './TeamHeadersSection.module.css';
 
 const TeamHeadersSection: React.FC = () => {
   return (
     <div className={classes.container}>
+      <TeamHeaderCard
+        verticalTitle={'Вертикаль БРД'}
+        userName={'Ерофеев Александр Петрович'}
+        userPosition={'Главный менеджер офиса\nуправления данными БРД и БПЛ и прочее'}
+        userAvatarFile={'Styupan_AS.jpg'}
+      />
+      <TeamHeaderCard
+        verticalTitle={'Вертикаль БРД'}
+        userName={'Ерофеев Александр Петрович'}
+        userPosition={'Главный менеджер офиса\nуправления данными БРД и БПЛ и прочее'}
+        userAvatarFile={'Styupan_AS.jpg'}
+      />
+      <TeamHeaderCard
+        verticalTitle={'Вертикаль БРД'}
+        userName={'Ерофеев Александр Петрович'}
+        userPosition={'Главный менеджер офиса\nуправления данными БРД и БПЛ и прочее'}
+        userAvatarFile={'Styupan_AS.jpg'}
+      />
+      <TeamHeaderCard
+        verticalTitle={'Вертикаль БРД'}
+        userName={'Ерофеев Александр Петрович'}
+        userPosition={'Главный менеджер офиса\nуправления данными БРД и БПЛ и прочее'}
+        userAvatarFile={'Styupan_AS.jpg'}
+      />
       <TeamHeaderCard
         verticalTitle={'Вертикаль БРД'}
         userName={'Ерофеев Александр Петрович'}
